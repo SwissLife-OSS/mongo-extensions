017a8c36ea9e512358b90e06e9c3cab1e5f2bb32
Normen Scheiber <46715105+nscheibe@users.noreply.github.com>
2019-12-04 14:53:48 +0100

    Added configuration for mongo options. (#6)
    Added first draft for mongo bootstrap sample.

---
+++ samples/Context/Business.Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ samples/Context/Business/OrderService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business
+{
+    class OrderService
+    {
+    }
+}

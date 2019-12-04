7a9ffe9d1b3a3837b6fd53d03d62d47bbd9fa99a
Normen Scheiber <nscheibe@live.com>
2019-12-04 16:54:21 +0100

    Added service collection extensions.

---
+++ samples/Context/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Order
+    {
+        public string Id { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
--- samples/Context/Models/Product.cs
+++ samples/Context/Models/Product.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Domain.Models
+namespace Models
 {
     public class Product
     {

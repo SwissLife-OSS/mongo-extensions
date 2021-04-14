09a644440cc11f06ca54acf47d34dc20c4e760dd
Normen Scheiber <nscheibe@live.com>
2021-04-14 17:27:34 +0200

    Corrected benchmark shell script.

--- benchmark/Prime.Extensions.Benchmarks.sh
+++ benchmark/Prime.Extensions.Benchmarks.sh
@@ -3,8 +3,8 @@
 rm -rf BenchmarkDotNet.Artifacts
 rm -rf Prime.Extensions.Benchmarks/bin
 rm -rf Prime.Extensions.Benchmarks/obj
-continue
+
 dotnet run --project Prime.Extensions.Benchmarks/ -c release
 
-continue
+
 

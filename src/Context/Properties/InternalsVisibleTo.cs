517f38afcf6700b3758cab31aa41fde8cc46b273
Tim Holzherr <tim.holzherr@swisslife.ch>
2021-10-11 15:27:33 +0200

    rename Context.Tests2 to Context.InterferingTests

--- src/Context/Properties/InternalsVisibleTo.cs
+++ src/Context/Properties/InternalsVisibleTo.cs
@@ -1,4 +1,4 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("MongoDB.Extensions.Context.Tests")]
-[assembly: InternalsVisibleTo("MongoDB.Extensions.Context.Tests2")]
+[assembly: InternalsVisibleTo("MongoDB.Extensions.Context.InterferingTests")]

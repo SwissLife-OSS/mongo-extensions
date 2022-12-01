e776ae4f17e58a8f0ebcae66acd5799060ef7db5
Normen Scheiber <46715105+nscheibe@users.noreply.github.com>
2022-12-01 11:22:17 +0100

    Implemented get mongo configuration via configuration section. (#69)

--- src/Context/MongoOptionsExtensions.cs
+++ src/Context/MongoOptionsExtensions.cs
@@ -1,38 +1,45 @@
-﻿using System;
+using System;
 
-namespace MongoDB.Extensions.Context
+namespace MongoDB.Extensions.Context;
+
+public static class MongoOptionsExtensions
 {
-    public static class MongoOptionsExtensions
+    public static MongoOptions<TMongoDBContext> Validate<TMongoDBContext>(
+        this MongoOptions<TMongoDBContext>? mongoOptions)
+        where TMongoDBContext : IMongoDbContext
     {
-        public static MongoOptions Validate(
-            this MongoOptions mongoOptions)
+        mongoOptions.Validate();
+
+        return mongoOptions!;
+    }
+
+    public static MongoOptions Validate(this MongoOptions? mongoOptions)
+    {
+        if (mongoOptions == null)
         {
-            if (mongoOptions == null)
-            {
-                throw new Exception(
-                    $"The MongoDB options for could not be found " +
-                    $"within the configuration section or the options are null.");
-            }
-
-            if (string.IsNullOrEmpty(mongoOptions.ConnectionString))
-            {
-                throw new Exception(
-                    $"The connection string of the MongoDB configuration " +
-                    $"could not be found within the configuration section. " +
-                    $"Please verify that this section contains the " +
-                    $"{nameof(MongoOptions.ConnectionString)} field.");
-            }
-
-            if (string.IsNullOrEmpty(mongoOptions.DatabaseName))
-            {
-                throw new Exception(
-                    $"The database name of the MongoDB configuration " +
-                    $"could not be found within the section " +
-                    $"Please verify that this section contains the " +
-                    $"{nameof(MongoOptions.DatabaseName)} field.");
-            }
-
-            return mongoOptions;
+            throw new Exception(
+                $"The MongoDB options for could not be found " +
+                $"within the configuration section or the options are null.");
         }
+
+        if (string.IsNullOrEmpty(mongoOptions.ConnectionString))
+        {
+            throw new Exception(
+                $"The connection string of the MongoDB configuration " +
+                $"could not be found within the configuration section. " +
+                $"Please verify that this section contains the " +
+                $"{nameof(MongoOptions.ConnectionString)} field.");
+        }
+
+        if (string.IsNullOrEmpty(mongoOptions.DatabaseName))
+        {
+            throw new Exception(
+                $"The database name of the MongoDB configuration " +
+                $"could not be found within the section " +
+                $"Please verify that this section contains the " +
+                $"{nameof(MongoOptions.DatabaseName)} field.");
+        }
+
+        return mongoOptions;
     }
 }

889cb9603a470fdd9067b69f929380eb6d93e3cb
glucaci <gabriel.lucaci.dev@gmail.com>
2023-10-02 17:35:10 +0200

    Remove the session provider without scope

--- src/Session.Abstractions/ISessionProvider.cs
+++ src/Session.Abstractions/ISessionProvider.cs
@@ -3,15 +3,11 @@ using System.Threading.Tasks;
 
 namespace MongoDB.Extensions.Session;
 
-public interface ISessionProvider
+public interface ISessionProvider<TScope>
 {
     Task<ITransactionSession> BeginTransactionAsync(
         CancellationToken cancellationToken);
 
     Task<ISession> StartSessionAsync(
         CancellationToken cancellationToken);
-}
-
-public interface ISessionProvider<TScope> : ISessionProvider
-{
 }

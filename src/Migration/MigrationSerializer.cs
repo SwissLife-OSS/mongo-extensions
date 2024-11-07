c057171d2fd51008f0f1e3ad63f405ec9aecd2e1
Mariusz Matysek <mariusz.matysek@swisslife.ch>
2024-11-07 09:02:30 +0100

    restored MigrationSerializer

--- src/Migration/MigrationSerializer.cs
+++ src/Migration/MigrationSerializer.cs
@@ -1,4 +1,3 @@
-using System;
 using MongoDB.Bson;
 using MongoDB.Bson.IO;
 using MongoDB.Bson.Serialization;
@@ -6,59 +5,37 @@ using MongoDB.Bson.Serialization.Serializers;
 
 namespace MongoDB.Extensions.Migration;
 
-class MigrationSerializer<T> : IBsonSerializer<T> where T : IVersioned
+class MigrationSerializer<T> : BsonClassMapSerializer<T> where T : IVersioned
 {
     private readonly EntityContext _context;
     private readonly MigrationRunner<T> _migrationRunner;
-    private readonly IBsonSerializer<T> _baseSerializer;
 
-    public MigrationSerializer(EntityContext context)
+    public MigrationSerializer(EntityContext context) : base(BsonClassMap.LookupClassMap(typeof(T)))
     {
         _context = context;
         _migrationRunner = new MigrationRunner<T>(context);
-        _baseSerializer = BsonSerializer.LookupSerializer<T>();
-
-        if (_baseSerializer is MigrationSerializer<T>)
-        {
-            throw new InvalidOperationException("The base serializer cannot be an instance of MigrationSerializer<T> to prevent recursion.");
-        }
     }
 
-    object IBsonSerializer.Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
-    {
-        return Deserialize(context, args);
-    }
 
-    public void Serialize(BsonSerializationContext context, BsonSerializationArgs args, object value)
-    {
-        if (value is T typedValue)
-        {
-            typedValue.Version = _context.Option.CurrentVersion;
-            _baseSerializer.Serialize(context, args, typedValue);
-        }
-        else
-        {
-            throw new ArgumentException($"Expected value to be of type {typeof(T)}, but was {value.GetType()}.", nameof(value));
-        }
-    }
-
-    public Type ValueType => typeof(T);
-
-    public void Serialize(BsonSerializationContext context, BsonSerializationArgs args, T value)
+    public override void Serialize(
+        BsonSerializationContext context,
+        BsonSerializationArgs args,
+        T value)
     {
         value.Version = _context.Option.CurrentVersion;
-        _baseSerializer.Serialize(context, args, value);
+        base.Serialize(context, args, value);
     }
 
-    public T Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+    public override T Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
     {
         BsonDocument bsonDocument = BsonDocumentSerializer.Instance.Deserialize(context);
 
         _migrationRunner.Run(bsonDocument);
 
-        var migratedContext = BsonDeserializationContext.CreateRoot(new BsonDocumentReader(bsonDocument));
+        var migratedContext =
+            BsonDeserializationContext.CreateRoot(new BsonDocumentReader(bsonDocument));
 
-        T entity = _baseSerializer.Deserialize(migratedContext, args);
+        T entity = base.Deserialize(migratedContext, args);
 
         entity.Version = _context.Option.CurrentVersion;
 

94ed856996be797dc635a4406176de4dedf25d36
Mariusz Matysek <mariusz.matysek@swisslife.ch>
2024-11-07 09:03:31 +0100

    cleanup

--- src/Context/TypeObjectSerializer.cs
+++ src/Context/TypeObjectSerializer.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
-using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Extensions.Context.Extensions;
 
